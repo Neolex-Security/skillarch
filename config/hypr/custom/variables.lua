b8d5163263837bc16e0bd75e045fde77353444fd
Neolex <hascoet.kevin@gmal.com>
2026-06-10 17:45:43 +0200

    switch from foot to kitty terminal by default

--- config/hypr/custom/variables.lua
+++ config/hypr/custom/variables.lua
@@ -1,1 +1,4 @@
+-- Custom overrides (survives dotfiles updates)
 
+-- Default terminal: kitty
+terminal = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'kitty -1' 'foot' 'alacritty' 'wezterm' 'konsole' 'kgx' 'uxterm' 'xterm'"
