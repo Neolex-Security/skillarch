0772a4589456c5713e846e497780ec3641ab3cd5
Neolex <hascoet.kevin@gmal.com>
2026-07-25 07:18:50 +0200

    remove hdmi

--- config/hypr/lua/monitors.lua
+++ config/hypr/lua/monitors.lua
@@ -3,5 +3,5 @@
 -- Disable the HDMI duplicate, keep DisplayPort (120Hz).
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 
-hl.monitor({ output = "HDMI-A-1", disabled = true })
 hl.monitor({ output = "DP-1", mode = "preferred", position = "0x0", scale = 1 })
+-- hl.monitor({ output = "HDMI-A-1", disabled = true })
